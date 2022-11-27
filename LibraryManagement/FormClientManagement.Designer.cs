baf8ef2f46ecd18eabfa47efbf6e4e2ee9b9dccc
idamou <othmane.idamou@emsi-edu.ma>
2022-11-27 10:28:13 +0100

    CLient Management ADD complete

---
+++ LibraryManagement/FormClientManagement.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace LibraryManagement
+{
+    partial class FormClientManagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxCIN = new System.Windows.Forms.TextBox();
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.labelCin = new System.Windows.Forms.Label();
+            this.buttonAjouter = new System.Windows.Forms.Button();
+            this.buttonModifier = new System.Windows.Forms.Button();
+            this.listViewClients = new System.Windows.Forms.ListView();
+            this.ID = new System.Windows.Forms.ColumnHeader();
+            this.Nom = new System.Windows.Forms.ColumnHeader();
+            this.Prenom = new System.Windows.Forms.ColumnHeader();
+            this.CIN = new System.Windows.Forms.ColumnHeader();
+            this.SuspendLayout();
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(56, 32);
+            this.textBoxFirstName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(132, 23);
+            this.textBoxFirstName.TabIndex = 0;
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(256, 32);
+            this.textBoxLastName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(141, 23);
+            this.textBoxLastName.TabIndex = 1;
+            // 
+            // textBoxCIN
+            // 
+            this.textBoxCIN.Location = new System.Drawing.Point(501, 32);
+            this.textBoxCIN.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxCIN.Name = "textBoxCIN";
+            this.textBoxCIN.Size = new System.Drawing.Size(112, 23);
+            this.textBoxCIN.TabIndex = 2;
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Location = new System.Drawing.Point(87, 15);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(64, 15);
+            this.labelFirstName.TabIndex = 3;
+            this.labelFirstName.Text = "First Name";
+            this.labelFirstName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(295, 15);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(63, 15);
+            this.labelLastName.TabIndex = 4;
+            this.labelLastName.Text = "Last Name";
+            // 
+            // labelCin
+            // 
+            this.labelCin.AutoSize = true;
+            this.labelCin.Location = new System.Drawing.Point(549, 15);
+            this.labelCin.Name = "labelCin";
+            this.labelCin.Size = new System.Drawing.Size(27, 15);
+            this.labelCin.TabIndex = 5;
+            this.labelCin.Text = "CIN";
+            // 
+            // buttonAjouter
+            // 
+            this.buttonAjouter.Location = new System.Drawing.Point(56, 85);
+            this.buttonAjouter.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonAjouter.Name = "buttonAjouter";
+            this.buttonAjouter.Size = new System.Drawing.Size(82, 22);
+            this.buttonAjouter.TabIndex = 6;
+            this.buttonAjouter.Text = "Ajouter";
+            this.buttonAjouter.UseVisualStyleBackColor = true;
+            this.buttonAjouter.Click += new System.EventHandler(this.buttonAjouter_Click);
+            // 
+            // buttonModifier
+            // 
+            this.buttonModifier.Location = new System.Drawing.Point(178, 85);
+            this.buttonModifier.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonModifier.Name = "buttonModifier";
+            this.buttonModifier.Size = new System.Drawing.Size(82, 22);
+            this.buttonModifier.TabIndex = 8;
+            this.buttonModifier.Text = "Modifier";
+            this.buttonModifier.UseVisualStyleBackColor = true;
+            this.buttonModifier.Click += new System.EventHandler(this.buttonModifier_Click);
+            // 
+            // listViewClients
+            // 
+            this.listViewClients.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ID,
+            this.Nom,
+            this.Prenom,
+            this.CIN});
+            this.listViewClients.FullRowSelect = true;
+            this.listViewClients.GridLines = true;
+            this.listViewClients.Location = new System.Drawing.Point(56, 111);
+            this.listViewClients.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listViewClients.MultiSelect = false;
+            this.listViewClients.Name = "listViewClients";
+            this.listViewClients.Size = new System.Drawing.Size(557, 218);
+            this.listViewClients.TabIndex = 9;
+            this.listViewClients.UseCompatibleStateImageBehavior = false;
+            this.listViewClients.View = System.Windows.Forms.View.Details;
+            this.listViewClients.SelectedIndexChanged += new System.EventHandler(this.listViewClient_SelectedIndexChanged);
+            // 
+            // ID
+            // 
+            this.ID.Text = "ID";
+            this.ID.Width = 50;
+            // 
+            // Nom
+            // 
+            this.Nom.Text = "Nom";
+            this.Nom.Width = 200;
+            // 
+            // Prenom
+            // 
+            this.Prenom.Text = "Prenom";
+            this.Prenom.Width = 200;
+            // 
+            // CIN
+            // 
+            this.CIN.Text = "CIN";
+            this.CIN.Width = 100;
+            // 
+            // FormClientManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 338);
+            this.Controls.Add(this.listViewClients);
+            this.Controls.Add(this.buttonModifier);
+            this.Controls.Add(this.buttonAjouter);
+            this.Controls.Add(this.labelCin);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.labelFirstName);
+            this.Controls.Add(this.textBoxCIN);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "FormClientManagement";
+            this.Text = "FormClientManagement";
+            this.Load += new System.EventHandler(this.FormClientManagement_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBoxFirstName;
+        private TextBox textBoxLastName;
+        private TextBox textBoxCIN;
+        private Label labelFirstName;
+        private Label labelLastName;
+        private Label labelCin;
+        private Button buttonAjouter;
+        private Button buttonModifier;
+        private ListView listViewClients;
+        private ColumnHeader ID;
+        private ColumnHeader Nom;
+        private ColumnHeader Prenom;
+        private ColumnHeader CIN;
+    }
+}
---
+++ LibraryManagement/FormClientManagement.cs
@@ -0,0 +1,85 @@
+﻿using LibraryManagment.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryManagement
+{
+    public partial class FormClientManagement : Form
+    {
+        private LibraryContext _libraryContext;
+        public FormClientManagement()
+        {
+            InitializeComponent();
+            _libraryContext = new LibraryContext();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void fillListViewClients()
+        {
+            listViewClients.Items.Clear();
+            foreach (Client client in _libraryContext.Clients.ToList())
+            {
+                ListViewItem listViewItem = new ListViewItem(client.Id.ToString());
+                listViewItem.SubItems.Add(client.Nom);
+                listViewItem.SubItems.Add(client.Prenom);
+                listViewItem.SubItems.Add(client.CIN);
+                listViewClients.Items.Add(listViewItem);
+            };
+
+        }
+
+        private void buttonAjouter_Click(object sender, EventArgs e)
+        {
+            if (textBoxFirstName.Text.Length == 0 || textBoxLastName.Text.Length == 0 || textBoxCIN.Text.Length == 0)
+            {
+                MessageBox.Show("Tous les champs sont obligatoire", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            _libraryContext.Clients.Add(new Client
+            {
+                Nom = textBoxFirstName.Text,
+                Prenom = textBoxLastName.Text,
+                CIN = textBoxCIN.Text,
+            });
+            _libraryContext.SaveChanges();
+
+            textBoxFirstName.Clear();
+            textBoxLastName.Clear();
+            textBoxCIN.Clear();
+            fillListViewClients();
+
+        }
+
+
+
+        private void buttonModifier_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listViewClient_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //_libraryContext.Clients.Find();
+            _libraryContext.Clients.Count(c => c.CIN == textBoxCIN.Text && c.Id != int.Parse(listViewClients.SelectedItems[0].SubItems[0].ToString()));
+        }
+
+
+        private void FormClientManagement_Load(object sender, EventArgs e)
+        {
+            fillListViewClients();
+        }
+
+        
+    }
+}
